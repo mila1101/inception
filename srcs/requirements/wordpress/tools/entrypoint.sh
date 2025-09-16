42297e376aeeea97e075029319aea136f3895c12
Mila <soklovackimilamasa1@gmail.com>
2025-09-16 11:57:47 +0200

    fixed url

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -8,8 +8,12 @@ USER_PASS=$(cat /run/secrets/wp_user_password)
 DB_NAME=${WORDPRESS_DB_NAME}
 DB_USER=${WORDPRESS_DB_USER}
 DB_HOST=${WORDPRESS_DB_HOST}
-SITE_TITLE=${TITLE:-INCEPTION}
-DOMAIN_NAME=${DOMAIN_NAME:-localhost}
+WP_TITLE=${WP_TITLE:-INCEPTION}
+WP_URL=${WP_URL:-https://localhost}
+WP_ADMIN_USER=${WP_ADMIN_USER:-msoklova}
+WP_ADMIN_EMAIL=${WP_ADMIN_EMAIL:-admin@example.com}
+WP_USER=${WP_USER:-editor_user}
+WP_USER_EMAIL=${WP_USER_EMAIL:-user@example.com}
 
 echo "(>‿◠) waiting for MariaDB at $DB_HOST..."
 until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" --silent; do
@@ -33,17 +37,17 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	echo "(^‿^) installing WordPress..."
 	wp core install --allow-root \
-		--url="http://$DOMAIN_NAME" \
-		--title="$SITE_TITLE" \
-		--admin_user="siteowner" \
+		--url="$WP_URL" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$ADMIN_PASS" \
-		--admin_email="admin@example.com" \
+		--admin_email="$WP_ADMIN_EMAIL" \
 		--path=/var/www/html
 
 	echo "(^‿^) creating second user..."
-	wp user create --allow-root writer writer@example.com \
+	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" \
 		--user_pass="$USER_PASS" \
-		--role=author \
+		--role=editor \
 		--path=/var/www/html
 fi
 
